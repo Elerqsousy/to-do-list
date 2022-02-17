7fb6d7740407fbc273867540a2b7a2d7a824696d
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-17 17:33:36 +0200

    Add content dynamically + delete content from index.html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
 import './style.css';
 import './modules/dependencies.js';
+import list from './modules/data.js';
+import icons from './modules/icons.js';
+
+const insertPoint = document.querySelector('.to-do-container');
+
+const insertlist = () => {
+  list.forEach((e) => {
+    const icon = () => {
+      if (e.completed) {
+        return icons.doneIcon.icon;
+      }
+      return icons.undoneIcon;
+    };
+    const linethrough = () => {
+      if (e.completed) {
+        return icons.doneIcon.linethrough;
+      } return null;
+    };
+    const listItem = `<li
+    class="padding-lr to-do-item display-flex-row height-50 border-bottom"
+  >
+    <div class="width-height display-flex-row width-80" id="${e.index}">
+      <button type="button" class="check" >
+        <i class="${icon()}"></i>
+      </button>
+      <input
+        class="display-flex-row width-height ${linethrough()}"
+        type="text"
+        value="${e.description}"
+      />
+    </div>
+    <button type="button" class="to-do-button-move">
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    </button>
+  </li>`;
+    insertPoint.innerHTML += listItem;
+  });
+};
+
+insertlist();

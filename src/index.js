726043c3f375762988918dcc128e5bef4a748356
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-19 02:13:52 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -4,66 +4,126 @@ import toDoList from './modules/data.js';
 import icons from './modules/icons.js';
 
 const insertPoint = document.querySelector('.to-do-container');
-let header = "Today's To Do";
 
-const listeners = () => {
-  const input = document.querySelectorAll('input');
-  input.forEach((e) => {
-    e.addEventListener('keyup', (event) => {
-      if (event.key === 'Enter') {
-        if (event.target.classList.contains('item')) {
-          const text = event.target.value;
-          const index = event.target.parentElement.id;
-          toDoList.edit(text, index);
-          populateStorageList();
-          insertlist();
+const populateStorageList = () => {
+  const stringList = JSON.stringify(toDoList.list);
+  localStorage.setItem('list', stringList);
+};
+
+const spitStorageList = () => {
+  const parsedList = JSON.parse(localStorage.getItem('list'));
+
+  for (let i = 0; i < parsedList.length; i += 1) {
+    const text = parsedList[i].description;
+    const trueFalse = parsedList[i].completed;
+    toDoList.add(text, trueFalse);
+  }
+};
+
+const insertlist = () => {
+  const listeners = () => {
+    const input = document.querySelectorAll('input');
+    input.forEach((e) => {
+      e.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+          if (event.target.classList.contains('item')) {
+            const text = event.target.value;
+            const index = e.parentElement.parentElement.id;
+            toDoList.edit(text, index);
+            populateStorageList();
+            insertlist();
+          }
         }
-      }
-    });
-    e.addEventListener('focus', (event) => {
-      if (event.target.classList.contains('item')) {
-        event.target.parentElement.parentElement.classList.add('marked');
-        const turnIconDelete = event.target.parentElement.nextElementSibling;
-        turnIconDelete.nextElementSibling.classList.remove('hidden');
-        turnIconDelete.classList.add('hidden');
-      }
+      });
+      e.addEventListener('focus', (event) => {
+        if (event.target.classList.contains('item')) {
+          event.target.parentElement.parentElement.classList.add('marked');
+          const turnIconDelete = event.target.parentElement.nextElementSibling;
+          turnIconDelete.nextElementSibling.classList.remove('hidden');
+          turnIconDelete.classList.add('hidden');
+        }
+      });
+
+      e.addEventListener('focusout', (event) => {
+        if (event.target.classList.contains('item')) {
+          event.target.parentElement.parentElement.classList.remove('marked');
+          const turnIcondots = event.target.parentElement.nextElementSibling;
+          const promise = new Promise((resolve) => {
+            setTimeout(() => resolve('done'), 100);
+          });
+          promise.then(() => {
+            turnIcondots.classList.remove('hidden');
+            turnIcondots.nextElementSibling.classList.add('hidden');
+          });
+        }
+      });
     });
 
-    e.addEventListener('focusout', (event) => {
-      if (event.target.classList.contains('item')) {
-        event.target.parentElement.parentElement.classList.remove('marked');
-        const turnIcondots = event.target.parentElement.nextElementSibling;
-        let promise = new Promise(function (resolve) {
-          setTimeout(() => resolve('done'), 100);
-        });
-        promise.then(() => {
-          turnIcondots.classList.remove('hidden');
-          turnIcondots.nextElementSibling.classList.add('hidden');
-        });
-      }
+    const trashIcon = document.querySelectorAll('.trash');
+    trashIcon.forEach((e) => {
+      e.addEventListener('click', () => {
+        const parentID = e.parentElement.id;
+        toDoList.pop(parseInt(parentID, 10));
+        populateStorageList();
+        insertlist();
+      });
     });
-  });
 
-  const trashIcon = document.querySelectorAll('.trash');
-  trashIcon.forEach((e) => {
-    e.addEventListener('click', () => {
-      const parentID = e.parentElement.id;
-      toDoList.pop(parseInt(parentID));
-      populateStorageList();
-      insertlist();
-    });
-  });
-
-  const checkBtn = document.querySelectorAll('.check');
-  checkBtn.forEach((btn) =>
-    btn.addEventListener('click', () => {
+    const checkBtn = document.querySelectorAll('.check');
+    checkBtn.forEach((btn) => btn.addEventListener('click', () => {
       const parentID = btn.parentElement.parentElement.id;
       toDoList.tog(parentID);
       populateStorageList();
 
       insertlist();
-    })
-  );
+    }));
+  };
+
+  const updateContent = () => {
+    insertPoint.innerHTML = '';
+
+    toDoList.list.forEach((e) => {
+      const icon = () => {
+        if (e.completed) {
+          return icons.doneIcon.icon;
+        }
+        return icons.undoneIcon;
+      };
+      const linethrough = () => {
+        if (e.completed) {
+          return icons.doneIcon.linethrough;
+        }
+        return null;
+      };
+      const listItem = `<li
+    class="padding-lr focus to-do-item display-flex-row height-50 border-bottom" id="${
+  e.index
+}"
+  >
+    <div class="width-height display-flex-row width-80" >
+      <button type="button" class="check" >
+        <i class="${icon()}"></i>
+      </button>
+      <input
+        class="item display-flex-row width-height ${linethrough()}"
+        type="text"
+        value="${e.description}"
+      />
+    </div>
+    <button type="button" class="to-do-button-move dots">
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    </button>
+    <button type="button" class="to-do-button-move trash hidden">
+      <i class="fa fa-trash"></i>
+    </button>
+  </li>`;
+      insertPoint.innerHTML += listItem;
+    });
+  };
+
+  updateContent();
+
+  listeners();
 };
 
 const staticListeners = () => {
@@ -89,8 +149,6 @@ const staticListeners = () => {
           populateStorageList();
 
           insertlist();
-        } else if (event.target.classList.contains('header')) {
-          header = event.target.value;
         }
         event.target.parentElement.classList.remove('marked');
       }
@@ -107,63 +165,6 @@ const staticListeners = () => {
       }
     });
   });
-};
-
-const populateStorageList = () => {
-  const stringList = JSON.stringify(toDoList.list);
-  localStorage.setItem('list', stringList);
-};
-
-const spitStorageList = () => {
-  const parsedList = JSON.parse(localStorage.getItem('list'));
-
-  for (let i = 0; i < parsedList.length; i++) {
-    let text = parsedList[i].description;
-    let trueFalse = parsedList[i].completed;
-    toDoList.add(text, trueFalse);
-  }
-};
-
-const insertlist = () => {
-  insertPoint.innerHTML = '';
-
-  toDoList.list.forEach((e) => {
-    const icon = () => {
-      if (e.completed) {
-        return icons.doneIcon.icon;
-      }
-      return icons.undoneIcon;
-    };
-    const linethrough = () => {
-      if (e.completed) {
-        return icons.doneIcon.linethrough;
-      } else return null;
-    };
-    const listItem = `<li
-    class="padding-lr focus to-do-item display-flex-row height-50 border-bottom" id="${
-      e.index
-    }"
-  >
-    <div class="width-height display-flex-row width-80" >
-      <button type="button" class="check" >
-        <i class="${icon()}"></i>
-      </button>
-      <input
-        class="item display-flex-row width-height ${linethrough()}"
-        type="text"
-        value="${e.description}"
-      />
-    </div>
-    <button type="button" class="to-do-button-move dots">
-      <i class="fa-solid fa-ellipsis-vertical"></i>
-    </button>
-    <button type="button" class="to-do-button-move trash hidden">
-      <i class="fa fa-trash"></i>
-    </button>
-  </li>`;
-    insertPoint.innerHTML += listItem;
-  });
-  listeners();
 };
 
 staticListeners();
--- src/modules/functionality.js
+++ src/modules/functionality.js
@@ -7,25 +7,25 @@ export default class Data {
   }
 
   add(item, finished) {
-    let newObject = new Node(item, finished);
+    const newObject = new Node(item, finished);
     newObject.object.index = this.count + 1;
     const newList = [...this.list, newObject.object];
     this.list = newList;
-    this.count++;
+    this.count += 1;
   }
 
   pop(index) {
     if (this.count !== 0) {
-      let newList = this.list.filter((e) => e.index !== index);
-      newList.forEach((e) => (e.index = newList.indexOf(e) + 1));
+      const newList = this.list.filter((e) => e.index !== index);
+      newList.forEach((e) => { e.index = newList.indexOf(e) + 1; });
       this.list = newList;
-      this.count--;
+      this.count -= 1;
     }
   }
 
   edit(text, index) {
     this.list.forEach((e) => {
-      if (e.index == index) {
+      if (e.index === parseInt(index, 10)) {
         e.description = text;
       }
     });
@@ -33,8 +33,8 @@ export default class Data {
 
   tog(index) {
     this.list.forEach((e) => {
-      if (e.index == index) {
-        if (e.completed == true) {
+      if (e.index === parseInt(index, 10)) {
+        if (e.completed === true) {
           e.completed = false;
         } else {
           e.completed = true;
